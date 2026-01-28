4fbb2ce1a4d4ccadd5da31791f1f8b30f936db6a
Aaditya Choudhary <aadityach812@gmail.com>
2026-01-28 14:20:13 +0530

    adding getAndSet file

---
+++ getAndSet.js
@@ -0,0 +1,35 @@
+
+//using get
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   language: "en",
+//   get lang() {
+//     return this.language;
+//   }
+// };
+// console.log(person.lang)
+
+//using set ...
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   language: "NO",
+//   set lang(value) {
+//     this.language = value;
+//   }
+// };
+// person.lang="hindi"
+// console.log(person.language)
+
+
+//example
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   language: "en",
+//   get lang() {
+//     return this.language.toUpperCase();
+//   }
+// };
+// console.log(person.lang)   //do not use parenthesis if using get
