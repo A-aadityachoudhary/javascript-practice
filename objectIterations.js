6cc42db17a88731955877c190900e76df5c7f0d5
Aaditya Choudhary <aadityach812@gmail.com>
2026-01-28 13:56:03 +0530

    adding objIteration file

---
+++ objectIterations.js
@@ -0,0 +1,24 @@
+const fruits = {Bananas:300, Oranges:200, Apples:500};
+// let text = "";
+// for (let [key,value] of Object.entries(fruits)){
+//     text += key + " : " + value + "   ";
+// }
+// console.log(text)
+
+// const myMap = new Map(Object.entries(fruits));
+// console.log(myMap);
+
+//using object.values()
+// let text = Object.values(fruits)
+// console.log(text)
+
+//using object.keys()
+// let text = Object.keys(fruits)
+// console.log(text)
+
+///iterate via loop
+// text = "";
+// for (let x in fruits){
+//     text += fruits[x] + " ";
+// }
+// console.log(text);
