25f9ea7a8cbaa68ab5691a3e32da7f3cbd07033c
Aaditya Choudhary <aadityach812@gmail.com>
2026-01-29 16:50:07 +0530

    file added

---
+++ object.ts
@@ -0,0 +1,23 @@
+// const car : {type:string, year:number}={
+//     type:"toyota",
+//     year:2345
+// };
+// console.log(car.year)
+
+
+////type inference
+// const car1 = {
+//     type:"safari"
+// };
+// car1.type = "lambo";
+// car1.type=23; //// show error cause type changed.
+// console.log(car1.type)
+
+
+// const car: { type: string, mileage?: number } = { // no error
+//   type: "Toyota"
+// };
+// console.log(car.mileage) //show undefined
+// car.mileage = 2000;
+// console.log(car.mileage) // show 2000
+
