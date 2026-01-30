a972bc360dab87ed1e11970316a032360b12bc1a
Aaditya Choudhary <aadityach812@gmail.com>
2026-01-30 10:26:10 +0530

    file added

---
+++ enum.ts
@@ -0,0 +1,10 @@
+enum direction {
+    east = 1,  ///if u will not write one it will take it as 0 
+    west ,
+    north,
+    south
+
+}
+console.log(direction.east)
+console.log(direction.west)  
+///values of enums means north etc can be strings also.
